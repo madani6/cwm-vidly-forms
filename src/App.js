0a21dd4aa6ec51ed7e7d5e6d760cc5c6fe23ae66
madani6 <anandamadani06@gmail.com>
2019-04-11 10:48:32 +0700

    register form

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import NotFound from "./components/notFound";
 import NavBar from "./components/navBar";
 import LoginForm from "./components/loginForm";
 import "./App.css";
+import RegisterForm from "./components/registerForm";
 
 class App extends Component {
   render() {
@@ -16,6 +17,7 @@ class App extends Component {
         <NavBar />
         <main className="container">
           <Switch>
+            <Route path="/register" component={RegisterForm} />
             <Route path="/login" component={LoginForm} />
             <Route path="/movies/:id" component={MovieForm} />
             <Route path="/movies" component={Movies} />
